e059a531bee05169fa38ed15398b2302b08e15dc
JUUDER <ashwin9526@gmail.com>
2026-08-26 02:09:28 +0530

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'newform-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './assets/hero.png',
+  './assets/mandhi.png',
+  './assets/alfaham.png',
+  './assets/beeffry.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
